51257e0048b93ca7b5503739ae1a1fc5a47c4b22
Ethan Blake <ethanblake417@gmail.com>
2023-08-24 14:38:59 -0700

    working on easy animation class.

--- matplotlib_add_ons/easy_animation.py
+++ matplotlib_add_ons/easy_animation.py
@@ -9,6 +9,8 @@ class FigureAnimation:
         self.redraw = redraw
         self.frames = frames
         self.interval = interval
+        self.animations = []
+        self.animate()
 
     def update(self, frame):
         new_data = self.get_new_data_func(frame)
@@ -17,20 +19,17 @@ class FigureAnimation:
             ax = fig.gca()
             if self.redraw:
                 ax.clear()
-
-            for line, (x, y) in zip(ax.get_lines(), data):
-                line.set_xdata(x)
-                line.set_ydata(y)
+                x, y = data[0]
+                ax.plot(x, y)
+            else:
+                for line, (x, y) in zip(ax.get_lines(), data):
+                    line.set_xdata(x)
+                    line.set_ydata(y)
         return ax.get_lines(),
 
     def animate(self):
-        animations = [FuncAnimation(fig, self.update, frames=self.frames, interval=self.interval) for fig in self.figures]
-        return animations
-
-    def save(self, filenames, writer='ffmpeg'):
-        animations = self.animate()
-        for animation, filename in zip(animations, filenames):
-            animation.save(filename, writer=writer)
+        for fig in self.figures:
+            self.animations.append(FuncAnimation(fig, self.update, frames=self.frames, interval=self.interval))
 
     def show(self):
         plt.show()
@@ -55,5 +54,5 @@ for _ in range(3):  # Creating 3 figures
     ax.plot(x, y)
     figures.append(fig)
 
-animation = FigureAnimation(figures, get_new_data_func=get_new_data, redraw=True, frames=100)
+animation = FigureAnimation(figures, get_new_data_func=get_new_data, redraw=True, interval=50)
 animation.show()
